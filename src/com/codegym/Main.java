2eb4d2d44574cd293817e85f26de7041a239a66c
minhhieu99a <hieunb99@gmail.com>
2022-02-11 16:06:28 +0700

    Students

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Students students1=new Students();
+        System.out.println(students1.setClasses("C05"));
+        System.out.println(students1.setName("Alex"));
+    }
+}
---
+++ src/com/codegym/Students.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class Students {
+        private String name="John";
+        private  String classes="C02";
+
+        Students (){
+        }
+        public String setName(String name){
+            return this.name=name;
+        }
+        public String setClasses(String classes){
+            return this.classes=classes;
+        }
+}
